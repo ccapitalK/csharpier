579e9c0331087e6ee92414235dfa14d33ceb0115
Bela VanderVoort <bela.vandervoort@optimizely.com>
2021-10-10 09:28:49 -0500

    Always break if there are two expressions for object initializers
    Closes #446

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/InitializerExpression.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/InitializerExpression.cs
@@ -11,18 +11,22 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
     {
         public static Doc Print(InitializerExpressionSyntax node)
         {
+            // TODO clean up this code if we keep it
+            // TODO handle this https://github.com/belav/csharpier-repos/pull/8/files#diff-21008119e248bd9f6fab1b1951077c06dfcbb1e311a364cf61375df336e54693
             Doc separator = node.Parent is AssignmentExpressionSyntax or EqualsValueClauseSyntax
                 ? Doc.Line
                 : Doc.Null;
+
+            var alwaysBreak = node.Kind() == SyntaxKind.ObjectInitializerExpression && node.Expressions.Count >= 2;
 
             var result = Doc.Concat(
                 separator,
                 Token.Print(node.OpenBraceToken),
                 Doc.Indent(
-                    Doc.Line,
-                    SeparatedSyntaxList.Print(node.Expressions, Node.Print, Doc.Line)
+                    alwaysBreak ? Doc.HardLine : Doc.Line,
+                    SeparatedSyntaxList.Print(node.Expressions, Node.Print, alwaysBreak ? Doc.HardLine : Doc.Line)
                 ),
-                Doc.Line,
+                alwaysBreak ? Doc.HardLine : Doc.Line,
                 Token.Print(node.CloseBraceToken)
             );
             return node.Parent
